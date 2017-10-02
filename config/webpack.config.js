6b1321197822e3ab27c22f1bb3faf979b39e423f
hugo <hcgar00@gmail.com>
2017-10-02 14:21:30 +0300

    Polish menu, fix history navigation and ctrl-c

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,13 +1,11 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
     entry: './src/js/index.js',
     module: {
-        rules: [{
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
+        rules: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
@@ -17,6 +15,20 @@ module.exports = {
                         presets: ['env', 'react']
                     }
                 }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'img/'
+                    }
+                }]
             }
         ]
     },
@@ -25,7 +37,9 @@ module.exports = {
         filename: 'index.js'
     },
     plugins: [
+        new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
+            favicon: './src/img/favicon.ico',
             filename: 'index.html',
             template: './src/template/index.html'
         })
--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,26 @@
-import 'normalize.css';
-import '../css/main.css';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import './analytics';
+import 'normalize.css';
+import '../css/main.css';
+
+const 
+    CMD_HELP = 'help',
+    CMD_WHOAMI = 'whoami',
+    CMD_GITHUB = 'github',
+    CMD_LINKEDIN = 'linkedin',
+    CMD_CLEAR = 'clear';
 
 class Terminal extends Component {
     constructor (props) {
         super(props);
         this.state = {
+            history: [],
+            historyIndex: -1,
             input: '',
-            inputHistory: [],
             lines: generateInitialInfo()
         };
+        this.keysPressed = [];
     }
 
     render() {
@@ -32,6 +42,8 @@ class Terminal extends Component {
                     value={input} 
                     onChange={this.onInputChange.bind(this)}
                     onKeyPress={this.onKeyPress.bind(this)}
+                    onKeyDown={this.onKeyDown.bind(this)}
+                    onKeyUp={this.onKeyUp.bind(this)}
                 />      
             </form>
         </div>;
@@ -42,68 +54,148 @@ class Terminal extends Component {
     }
 
     onInputChange(event) {
-        this.setState({input: event.target.value});
+        this.setState({
+            input: event.target.value,
+            historyIndex: -1
+        });
     }
 
     onKeyPress(event) {
-        switch (event.key) {
-            case 'Enter': 
-                const input = this.state.input;
-                const command = getCommand(input);
-                const newLines = generateOutput(input);
-
-                const oldLines = command === 'clear' ? [] : this.state.lines;
-
-                this.setState({
-                    input: '',
-                    inputHistory: [...this.state.inputHistory, input],
-                    lines: [
-                        ...oldLines, 
-                        '> ' + input, 
-                        ...newLines
-                    ]
-                });
-            default:
+        if (event.key === 'Enter') {
+            this.processInput();
         }
     }
-}
+
+    processInput() {
+        const input = this.state.input.trim();
+        const command = getCommand(input);
+        const newLines = generateOutput(input);
+
+        const oldLines = command === CMD_CLEAR ? [] : this.state.lines;
+
+        this.setState({
+            input: '',
+            history: [...this.state.history, input].filter(i => i !== ''),
+            historyIndex: -1,
+            lines: [
+                ...oldLines, 
+                '> ' + input, 
+                ...newLines
+            ]
+        });
+    }
+
+    onKeyDown(event) {
+        this.keysPressed[event.keyCode] = true;
+        // Ctrl + c
+        if (this.keysPressed[17] && this.keysPressed[67]) {
+            this.processInterrupt();
+        }
+        // Up arrow
+        else if (event.keyCode === 38) {
+            this.navigateHistoryUp();
+        } 
+        // Down arrow
+        else if (event.keyCode === 40){
+            this.navigateHistoryDown();
+        }
+    }
+
+    processInterrupt() {
+        this.setState({
+            input: '',
+            historyIndex: -1,
+            lines: [
+                ...this.state.lines, 
+                '> ' + this.state.input
+            ]
+        });
+    }
+
+    onKeyUp(event) {
+        this.keysPressed[event.keyCode] = false;
+    }
+
+    navigateHistoryUp() {
+        const { history, historyIndex } = this.state;
+        
+        let newIndex;
+        if (historyIndex === -1) {
+            newIndex = history.length - 1;
+        } else {
+            newIndex = Math.max(0, historyIndex - 1);
+        }
+
+        this.setState({
+            input: history[newIndex],
+            historyIndex: newIndex
+        });
+    }
+
+    navigateHistoryDown() {
+        const { history, historyIndex } = this.state;
+
+        let newIndex;
+        if (historyIndex === -1) {
+            return;
+        } else {
+            newIndex = Math.min(history.length - 1, historyIndex + 1);
+        }
+
+        this.setState({
+            input: history[newIndex],
+            historyIndex: newIndex
+        });
+    }
+};
 
 const getCommand = input => {
-    const items = input.split(' ');
+    const items = input.split(' ').filter(i => i !== '');
     if (items.length === 0) {
         return undefined;
     }
     return items.shift();
-}
+};
 
 const generateOutput = input => {
     const command = getCommand(input);
-    const items = input.split(' ').slice(1);
+    const args = input.split(' ').slice(1);
     switch (command) {
-        case '':
+        case undefined:
             return [];
-        case 'help':
+        case CMD_HELP:
             return generateHelp();
-        case 'hello':
-            return ['Hello there, Mr/Mrs ' + items.join(' ') + '!'];
-        case 'github':
+        case CMD_GITHUB:
             return generateGithub();
-        case 'linkedin':
+        case CMD_LINKEDIN:
             return generateLinkedIn();
-        case 'whoami':
+        case CMD_WHOAMI:
             return generatePersonalInfo();
-        case 'clear':
+        case CMD_CLEAR:
             return [];
         default:
-            return [`Unknown command "${command}"`];
+            return [`Command not found: ${command}`];
     }
-}
+};
 
 const generateInitialInfo = () => {
     return [
+        'Welcome to the internet!',
         'Type \'help\' command to show the help menu'
     ];
-}
+};
+
+const generateHelp = () => {
+    return [
+        ` `,
+        `   ${CMD_HELP}           Show help menu`,
+        `   ${CMD_WHOAMI}         Show personal info`,
+        `   ${CMD_GITHUB}         Go to my Github page`,
+        `   ${CMD_LINKEDIN}       Go to my LinkedIn page`,
+        `   ${CMD_CLEAR}          Clear terminal output`,
+        ` `
+    ];
+};
 
 const generatePersonalInfo = () => {
     return [
@@ -111,34 +203,21 @@ const generatePersonalInfo = () => {
         'I am a software engineer living in Helsinki, Finland.',
         'I love to write code.'
     ];
-}
-
-const generateHelp = () => {
-    return [
-        ' ',
-        '   help           Show help menu',
-        '   hello <name>   Show a personalized greeting',
-        '   github         Go to my Github page',
-        '   linkedin       Go to my LinkedIn page',
-        '   whoami         Show personal info',
-        '   clear          Clear terminal output',
-        ' '
-    ];
-}
+};
 
 const generateGithub = () => {
     window.open('https://github.com/hugo-cardenas');
     return [
         'This is my Github page: https://github.com/hugo-cardenas'
     ];
-}
+};
 
 const generateLinkedIn = () => {
     window.open(' https://linkedin.com/in/hugocardenas/?locale=en_US');
     return [
         'This is my LinkedIn page: https://linkedin.com/in/hugocardenas/?locale=en_US'
     ];
-}
+};
 
 ReactDOM.render(
     <Terminal/>,
